a5de54d0c3fc00aa1587fe9d3ec8f9edb3820ebb
kuluruvineeth <kuluruvineeth8623@gmail.com>
2021-08-31 10:23:06 +0530

    react state added initially

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,118 @@
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const helloContinents = Array.from(continents,c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
+const initialIssues = [
+  {
+    id: 1, status: 'New', owner: 'Ravan', effort: 5,
+    created: new Date('2018-08-15'),due:undefined,
+    title:'Error in console when clicking Add',
+  },
+  {
+    id:2,status:'assigned',owner:'Eddie',effort:14,
+    created: new Date('2018-08-16'),due: new Date('2018-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
 
 
-const element = (
-  <div title="Outer div">
-  <h1>{message}</h1>
-  </div>
-);
+class IssueFilter extends React.Component{
+  render(){
+    return(
+      <div>This is a placeholder for the issue filter.</div>
+    );
+  }
+}
+
+class IssueRow extends React.Component{
+  render(){
+    const issue = this.props.issue
+    return(
+      <tr>
+       <td>{issue.id}</td>
+       <td>{issue.status}</td>
+       <td>{issue.owner}</td>
+       <td>{issue.created.toDateString()}</td>
+       <td>{issue.effort}</td>
+       <td>{issue.due ? issue.due.toDateString() : ''}</td>
+       <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
+
+class IssueTable extends React.Component{
+  constructor(){
+    super();
+    this.state = {issues: []};
+  }
+
+  componentDidMount(){
+    this.loadData();
+  }
+
+  loadData(){
+    setTimeout(() => {
+      this.setState({issues: initialIssues});
+    },500);
+  }
+  render(){
+    const issueRows = this.state.issues.map(issue => <IssueRow key={issue.id} issue={issue}/>);
+    return(
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Due Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {issueRows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class IssueAdd extends React.Component{
+  render(){
+    return(
+      <div>This is a placeholder for a form to add an issue</div>
+    );
+  }
+}
+
+class IssueList extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
+
+    );
+  }
+}
+
+// class HelloWorld extends React.Component{
+  
+//   render(){
+//     const continents = ['Africa','America','Asia','Australia','Europe'];
+//     const helloContinents = Array.from(continents,c => `Hello ${c}!`);
+//     const message = helloContinents.join(' ');
+//     return(
+//       <div title="Outer div">
+//       <h1>{message}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+const element = <IssueList />;
 
 ReactDOM.render(element,document.getElementById('content'));
